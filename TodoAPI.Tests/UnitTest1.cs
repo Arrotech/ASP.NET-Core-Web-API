100f6f71c45257a541f97cac7b1e5e1abe3821e2
Harun Gachanja <arrotechdesign@gmail.com>
2021-06-04 15:55:58 +0300

    Chore (Set Up Test Project)
    What does thid PR do?
    
    Set up the test project to test the given API endpoints.

---
+++ TodoAPI.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoApi.Models;
+
+namespace TodoAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
